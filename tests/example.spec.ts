782e998b268ac65d16d72197a085c9c16889ced8
”Egzona <Egze_14@hotmail.com>
2024-08-27 13:58:13 +0200

    first

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -13,3 +13,18 @@ test('test', async ({ page }) => {
   await page.getByRole('link', { name: 'Blog' }).click();
   await expect(page.getByRole('heading', { name: 'Latest Blog' })).toBeVisible();
 });
+
+
+
+
+test('test', async ({ page }) => {
+  await page.goto('http://127.0.0.1:5500/index.html');
+  await page.getByRole('link', { name: 'Blog' }).click();
+  await expect(page.getByText('iusmod tempor incididunt ut').first()).toBeVisible();
+  await page.getByRole('link', { name: 'Classes' }).click();
+  await expect(page.getByText('iusmod tempor incididunt ut')).toBeVisible();
+  await page.getByRole('link', { name: 'Home (current)' }).click();
+  await page.getByRole('heading', { name: 'Boxing Center' }).click();
+  await page.getByRole('heading', { name: 'Boxing Center' }).click();
+  await expect(page.getByRole('heading', { name: 'Boxing Center' })).toBeVisible();
+});
